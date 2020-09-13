6b37e9d693f9fb615dc82451989b23bfd6082ad1
dominikk19 <dominikk19@gmail.com>
2020-09-13 19:55:57 +0200

    multi pass encoder

--- src/main/java/com/example/springsecurity/config/SecurityConfiguration.java
+++ src/main/java/com/example/springsecurity/config/SecurityConfiguration.java
@@ -1,8 +1,20 @@
 package com.example.springsecurity.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Dominik Kiszka {dominikk19}
@@ -18,8 +30,39 @@ class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .csrf()
                 .disable()
                 .authorizeRequests(authorizeRequest -> authorizeRequest
+                        .antMatchers(HttpMethod.POST, "/pizza").hasRole("ADMIN")
+                        .antMatchers(HttpMethod.GET, "/test", "/pizza/").hasRole("USER")
                         .anyRequest()
                         .permitAll());
-
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(standardPasswordEncoder().encode("admin")).authorities(List.of(new SimpleGrantedAuthority("ADMIN")))
+                .and()
+                .withUser("user")
+                .password(pbkdf2PasswordEncoder().encode("password")).authorities(List.of(new SimpleGrantedAuthority("USER")));
+    }
+
+    @Bean
+    public PasswordEncoder delegatingPasswordEncoder() {
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("string", standardPasswordEncoder());
+        encoders.put("pbkdf2", pbkdf2PasswordEncoder());
+        DelegatingPasswordEncoder delegatingPasswordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
+        delegatingPasswordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2PasswordEncoder());
+        return delegatingPasswordEncoder;
+    }
+
+    private PasswordEncoder standardPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    private PasswordEncoder pbkdf2PasswordEncoder() {
+        return new Pbkdf2PasswordEncoder();
+    }
+
+
 }
